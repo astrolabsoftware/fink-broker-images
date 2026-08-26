fb775bae8241fc513c677e81e2e707db97806ee0
JulienPeloton <peloton@lal.in2p3.fr>
2026-08-26 14:49:56 +0200

    Bring back SFD maps initialisation to default (Harvard Dataverse fixed)

--- containerfs/install_python_deps.sh
+++ containerfs/install_python_deps.sh
@@ -127,35 +127,7 @@ elif [[ "$MODE" == "science" ]]; then
     # ZTF-specific: dustmaps initialization
     if [[ "$SURVEY" == "ztf" ]]; then
         echo "Initializing dustmaps for ZTF..."
-        python -c "
-from dustmaps.config import config
-import dustmaps.sfd
-import json
-import requests
-import os
-
-# Copied from website https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/EWCNL5
-fallback_url_ngp = 'https://dvn-cloud-iqss.s3.amazonaws.com/10.7910/DVN/EWCNL5/157bb43e038-2856c7e44170?response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27SFD_dust_4096_ngp.fits&response-content-type=application%2Ffits&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20260814T142536Z&X-Amz-SignedHeaders=host&X-Amz-Credential=AKIAZT3GWQ6FKBSH5I56%2F20260814%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Expires=3600&X-Amz-Signature=4297c294926073694745a74d3b92660178460b2e67c54704052f7d644b1c6be1'
-fallback_url_sgp = 'https://dvn-cloud-iqss.s3.amazonaws.com/10.7910/DVN/EWCNL5/157bb8a4df2-7537b98d9920?response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27SFD_dust_4096_sgp.fits&response-content-type=application%2Ffits&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20260814T142611Z&X-Amz-SignedHeaders=host&X-Amz-Credential=AKIAZT3GWQ6FKBSH5I56%2F20260814%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Expires=3600&X-Amz-Signature=17ca3601ced076cae04944ec0ca147fda6e19ceb143a4bd8b6f17ab50e904d9f'
-
-fname_ngp = '/opt/fink-broker/miniconda/lib/python3.9/site-packages/dustmaps/data/sfd/SFD_dust_4096_ngp.fits'
-fname_sgp = '/opt/fink-broker/miniconda/lib/python3.9/site-packages/dustmaps/data/sfd/SFD_dust_4096_sgp.fits'
-try:
-    dustmaps.sfd.fetch()
-except json.decoder.JSONDecodeError:
-    r_ngp = requests.get(fallback_url_ngp)
-    r_sgp = requests.get(fallback_url_sgp)
-    
-    # Make sure the directory it's going into exists
-    dir_name = os.path.dirname(fname_ngp)
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
-
-    with open(fname_ngp, 'wb') as f:
-        f.write(r_ngp.content)
-    with open(fname_sgp, 'wb') as f:
-        f.write(r_sgp.content)
-"
+        python -c "from dustmaps.config import config;import dustmaps.sfd;dustmaps.sfd.fetch()"
     fi
 fi
 
